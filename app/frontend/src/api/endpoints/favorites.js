729051cb896d0184c00624963e47d590c597c2c1
Pavlo Zinchenko <pavlozinchenko.pz@gmail.com>
2024-09-04 00:02:49 +0200

    Add favorites API endpoint to fetch all chosen

--- app/frontend/src/api/endpoints/favorites.js
+++ app/frontend/src/api/endpoints/favorites.js
@@ -1,5 +1,9 @@
 import axiosInstance from '../axiosConfig';
 
+export const getFavorites = () => {
+  return axiosInstance.get('/favorites');
+}
+
 export const addFavorite = (artistId) => {
   return axiosInstance.post(`/favorites/${artistId}`);
 }
