449ea75cf52599deec2987bea0758f4cc1aca45e
Pavlo Zinchenko <pavlozinchenko.pz@gmail.com>
2024-09-05 09:59:27 +0200

    Update axiosConfig.js to use correct baseURL and more endpoints

--- app/frontend/src/api/axiosConfig.js
+++ app/frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3030/api',
+    baseURL: 'http://localhost:3030',
     timeout: 2500,
     headers: { 'Content-Type': 'application/json' },
 });
