5c075d8ad91ddd1a51266664be5757091cf9a86d
Pavlo Zinchenko <pavlozinchenko.pz@gmail.com>
2024-09-05 09:55:13 +0200

    Fix DB connection

--- app/backend/src/db/index.js
+++ app/backend/src/db/index.js
@@ -4,9 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
     },
 });
 
