a2d7fb8a25383d9f3aa6bef168706d1db82c67a1
Pavlo Zinchenko <pavlozinchenko.pz@gmail.com>
2024-09-05 11:44:22 +0200

    Update routing in App component and remove unused code in HomePage component

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import HomePage from './pages/HomePage';
+import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
+import ArtistsList from './components/ArtistsList';
+import ArtistDetail from './components/ArtistDetail';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<ArtistsList />} />
+        <Route path="/artists/:artistId" element={<ArtistDetail />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
--- app/frontend/src/pages/HomePage.jsx
+++ app/frontend/src/pages/HomePage.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import { useSelector, useDispatch } from 'react-redux';
 
 export default function HomePage() {
@@ -11,17 +8,13 @@ export default function HomePage() {
   const decrement = () => ({ type: 'counter/decrement' });
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Header />
-      <main style={{ flexGrow: 1, padding: '20px', textAlign: 'center' }}>
-        <h1>Welcome to the Home Page</h1>
-        <div>
-          <h1>Counter: {count}</h1>
-          <button onClick={() => dispatch(increment())}>Increment</button>
-          <button onClick={() => dispatch(decrement())}>Decrement</button>
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <main style={{ flexGrow: 1, padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to the Home Page</h1>
+      <div>
+        <h1>Counter: {count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </main>
   );
 }
