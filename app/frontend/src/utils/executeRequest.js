79bffdba9abad3368fc430453348868e9d2a8373
Pavlo Zinchenko <pavlozinchenko.pz@gmail.com>
2024-09-08 14:09:51 +0200

    Remove unused success messages after all executing all requests

--- app/frontend/src/utils/executeRequest.js
+++ app/frontend/src/utils/executeRequest.js
@@ -1,17 +1,14 @@
-import { notifySuccess, notifyError } from '@utils/ToastNotifications';
+import { notifyError } from '@utils/ToastNotifications';
 
-export const executeRequest = async (requestFunction, successMessage) => {
+export const executeRequest = async (requestFunction) => {
     try {
         const response = await requestFunction();
-        if (successMessage) {
-            notifySuccess(successMessage);
-        }
         return response.data;
     } catch (error) {
         if (error.response && error.response.data) {
             notifyError(error.response.data.message);
         } else {
-            notifyError('Something went wrong, please try again.');
+            notifyError('Something went wrong :(');
         }
         throw error;
     }
